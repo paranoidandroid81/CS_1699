1a43eabc5dee0ad5da92c43b837b2c0a8ba784b3
paranoidandroid81 <mpk44@pitt.edu>
2018-09-19 11:26:36 -0400

    Moved sample code to main D1 dir. Removed unnecessary Laboon code

--- sample_code/basic_blockchain/HashableObject.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interface for all HashableObjects.
- * Any object which a HashPointer will point to must
- * obey this interface.
- */
-
-public interface HashableObject {
-    public String getDataAsString();
-}
