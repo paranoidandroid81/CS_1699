c89a55b399bb2da7a4331352e851afec251c78f2
paranoidandroid81 <mpk44@pitt.edu>
2018-09-19 22:02:58 -0400

    Began coding

--- Deliverable_1/StringCoin.java
+++ Deliverable_1/StringCoin.java
@@ -2,12 +2,34 @@
  * Implementation of centralized StringCoin cryptocurrency
  */
 
+ import java.io.*;
+ import java.security.*;
+ import java.security.spec.*;
+
  public class StringCoin
  {
    public static void main(String[] args)
    {
-     
+     BufferedReader br;
+     try
+     {
+       //create reader to read from blockchain file
+       br = new BufferedReader(new FileReader(args[0]));
+       String line = br.readLine();
+       int lines = 0;
+       while (line != null)         //count lines in file
+       {
+         lines++;
+         line = br.readLine();
+       }
+     } catch (IOException e)
+     {
+       System.err.println(e);
+       System.exit(1);        //something wrong with input file, exit
+     }
+     br.close();        //close file for now
+     //now that we have number of lines (num of blocks), we can build blockchain
+     Blockchain blockchain = new Blockchain();
+     Block[] blocks = new Block[lines];
    }
-
-
- }
+}
